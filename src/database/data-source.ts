26a32c1fa4580a2adfa04f4e3bc417262a250d4f
flaman cadeau <irakozeflamanc@gmail.com>
2026-01-20 20:41:46 +0200

    fix: add DATABASE_URL for production connection

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -8,11 +8,16 @@ const isProd = process.env.NODE_ENV === 'production';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
+
+  url: process.env.DATABASE_URL,
+
+
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
 
   entities: isProd ? ['dist/**/*.entity.js'] : ['src/**/*.entity.ts'],
   migrations: isProd ? ['dist/migrations/*.js'] : ['src/migrations/*.ts'],
