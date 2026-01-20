6a76fbaac74f0a2d573340821556f303553a1869
flaman cadeau <irakozeflamanc@gmail.com>
2026-01-20 21:16:47 +0200

    fix:bind server to 0.0.0.0 and use env PORT

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   );
 
   setupSwagger(app);
-  await app.listen(port);
+  await app.listen(port,'0.0.0.0');
 
   console.log(`🚀 Application is running on: http://localhost:${port}`);
   console.log(`📄 Swagger docs at http://localhost:${port}/api`);
